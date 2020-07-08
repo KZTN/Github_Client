346ab8a8d38d5b4c5284b3e8d61eea54a25363fe
KZTN <kakaiocesar2@gmail.com>
2020-07-08 16:46:42 -0300

    :sparkles: Added form elements and stylization
    all changes are functional

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import {Text} from 'react-native';
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
-  return <Text>Main</Text>;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Usuário Github"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
